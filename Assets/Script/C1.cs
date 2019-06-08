8c9f2c6a9e034fab3ffd3f9b47f825e534a36731
Saeed <skarimy73@gmail.com>
2019-06-08 17:44:55 +0430

    Initial Commit

---
+++ Assets/Script/C1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class C1 : MonoBehaviour
+{
+	public Vector2 number;
+	public List<C1> Connctedonboard = new List<C1>();
+	public List<C1> Connectedbywire = new List<C1>();
+	internal float Voltage = -1;
+	internal float Current = -1;
+	internal int tool;
+
+	public void Addconnection(C1 connectedNode)
+	{
+		
+		if (GameManager.instance.state == 3)
+		{
+			Connectedbywire.Add((connectedNode));
+			if (GameManager.instance.selectedtool == 1)
+			{
+				this.Current = (this.Voltage - connectedNode.Voltage) / 10;
+				connectedNode.Current = -(this.Current);
+			}
+			Connectedbywire.Add((connectedNode));
+			if (GameManager.instance.selectedtool == 2)
+			{
+				this.Current = (this.Voltage - connectedNode.Voltage) / 100;
+				connectedNode.Current = -(this.Current);
+			}
+			Connectedbywire.Add((connectedNode));
+			if (GameManager.instance.selectedtool == 3)
+			{
+				this.Current = (this.Voltage - connectedNode.Voltage) / 1000;
+				connectedNode.Current = -(this.Current);
+			}
+			GameManager.instance.state = 0;
+			Debug.Log(number + "Connection by wire to" + Connectedbywire[Connectedbywire.Count-1].number );
+		}
+		else
+		{
+			Connctedonboard.Add(connectedNode);
+		}
+	}
+
+	public void SetVoltage(float v)
+	{
+		for (int i = 0; i < Connctedonboard.Count; i++)
+		{
+			Connctedonboard[i].Voltage = v;
+		}
+	}
+	/*public void SetCurrent(float c)
+	{
+		for (int i = 0; i < Connctedonboard.Count; i++)
+		{
+			Connctedonboard[i].Current = c;
+		}
+	}*/
+	
+	// Use this for initialization
+	private void OnMouseDown()
+	{
+		if (GameManager.instance.state == 0)
+		{
+			Debug.Log(" tool not selected");
+		}
+		else if (GameManager.instance.state == 1)
+		{
+			GameManager.instance.pins[0] = number;
+			GameManager.instance.state = 2;
+			Debug.Log("first pin choosed" + number);
+		}
+			
+		else if (GameManager.instance.state == 2)
+		{
+			GameManager.instance.pins[1] = number;
+			Debug.Log("second pin choosed" + number);
+			GameManager.instance.state = 3;
+			GameManager.instance.connect2pins();
+		}
+			
+	}
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.Serialization;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	public static GameManager instance;
+	public int state, selectedtool;
+	internal GameObject[,] board;
+	public GameObject cube;
+	public GameObject rescube;
+	public GameObject res2cube;
+	public GameObject res3cube;
+	public Vector2[] pins;
+	
+	void Start ()
+	{
+		instance = this;
+		board = new GameObject[30,7];
+		pins = new Vector2[2];
+		state = 0;
+		selectedtool = 0;
+		
+		for (int j = 0; j < 7; j++)
+		{
+			for (int i = 0; i < 30; i++)
+			{
+				if (j < 5)
+				{
+					GameObject g0 = Instantiate(cube, new Vector3(i * 1.1F - 15, 11.5F, j * 1.3F), Quaternion.identity);
+					board[i, j] = g0;
+					g0.GetComponent<C1>().number = new Vector2(i, j);
+				}
+				else
+				{
+					GameObject g0 = Instantiate(cube, new Vector3(i * 1.1F - 15, 11.5F, 1F + j * 1.3F), Quaternion.identity);
+					board[i, j] = g0;
+					g0.GetComponent<C1>().number = new Vector2(i, j);
+
+				}
+			}	
+		}
+		for (int j = 0; j < 5; j++)
+			for (int i = 0; i < 30; i++)
+				for (int k = 0; k < 5; k++)
+					if (k != j)
+						board[i,j].GetComponent<C1>().Addconnection(board[i,k].GetComponent<C1>());
+		for (int j = 5; j < 7; j++)
+			for (int i = 0; i < 30; i++)
+				for (int k = 0; k < 30; k++)
+					if (k != i)
+						board[i,j].GetComponent<C1>().Addconnection(board[k,j].GetComponent<C1>());
+		board[0,6].GetComponent<C1>().SetVoltage(10F);
+	}
+
+	private void OnMouseDown()
+	{
+		
+	}
+
+	internal void connect2pins()
+	{
+		float rotateAngle;
+		Vector3 middlepos = new Vector3();
+		Vector3 firstpin = board[(int) pins[0].x, (int) pins[0].y].transform.position;
+		Vector3 secondpin = board[(int) pins[1].x, (int) pins[1].y].transform.position;
+		rotateAngle = -(firstpin.z - secondpin.z) / (firstpin.x - secondpin.x);
+		middlepos = (firstpin + secondpin) / 2;
+		middlepos.y += 1;
+		if (GameManager.instance.selectedtool == 1)
+		{
+			GameObject g0 = Instantiate(rescube, middlepos,Quaternion.identity);
+			g0.transform.localScale = new Vector3(Mathf.Sqrt((firstpin.x - secondpin.x)*(firstpin.x - secondpin.x) + (firstpin.z - secondpin.z)*(firstpin.z - secondpin.z)),1,1);
+			g0.transform.localRotation = Quaternion.Euler(0,Mathf.Rad2Deg*Mathf.Atan(rotateAngle),0);
+			board[(int) pins[0].x, (int) pins[0].y].GetComponent<C1>().Addconnection(board[(int) pins[1].x, (int) pins[1].y].GetComponent<C1>());
+		}
+		else if (GameManager.instance.selectedtool == 2)
+		{
+			GameObject g0 = Instantiate(res2cube, middlepos,Quaternion.identity);
+			g0.transform.localScale = new Vector3(Mathf.Sqrt((firstpin.x - secondpin.x)*(firstpin.x - secondpin.x) + (firstpin.z - secondpin.z)*(firstpin.z - secondpin.z)),1,1);
+			g0.transform.localRotation = Quaternion.Euler(0,Mathf.Rad2Deg*Mathf.Atan(rotateAngle),0);
+			board[(int) pins[0].x, (int) pins[0].y].GetComponent<C1>().Addconnection(board[(int) pins[1].x, (int) pins[1].y].GetComponent<C1>());
+		}
+		else if (GameManager.instance.selectedtool == 3)
+		{
+			GameObject g0 = Instantiate(res3cube, middlepos,Quaternion.identity);
+			g0.transform.localScale = new Vector3(Mathf.Sqrt((firstpin.x - secondpin.x)*(firstpin.x - secondpin.x) + (firstpin.z - secondpin.z)*(firstpin.z - secondpin.z)),1,1);
+			g0.transform.localRotation = Quaternion.Euler(0,Mathf.Rad2Deg*Mathf.Atan(rotateAngle),0);
+			board[(int) pins[0].x, (int) pins[0].y].GetComponent<C1>().Addconnection(board[(int) pins[1].x, (int) pins[1].y].GetComponent<C1>());
+		}
+		
+	}
+	
+	public void ItemClick(int item)
+	{
+		if (item == 0)
+		{
+			GameManager.instance.state = 1;
+			GameManager.instance.selectedtool = 1;
+			Debug.Log("clicked on resistance1");
+		}		
+		if (item == 1)
+		{
+			GameManager.instance.state = 1;
+			GameManager.instance.selectedtool = 2;
+			Debug.Log("clicked on resistance2");
+		}		
+		if (item == 2)
+		{
+			GameManager.instance.state = 1;
+			GameManager.instance.selectedtool = 3;
+			Debug.Log("clicked on resistance3");
+		}		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
